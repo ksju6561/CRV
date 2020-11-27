23a873537531140224e224a684b81fe7fd3dd395
AALS7501 <shparkk95@kookmin.ac.kr>
2020-11-27 16:54:39 +0900

    Elliptic Curve added

--- JsnarkCircuitBuilder/src/examples/generators/Vote.java
+++ JsnarkCircuitBuilder/src/examples/generators/Vote.java
@@ -1,37 +1,29 @@
 /*******************************************************************************
- * Author: Seongho Park <shparkk95@kookmin.ac.kr>
+ * Author: Jaekyoung Choi <cjk2889@kookmin.ac.kr>
  *******************************************************************************/
 package examples.generators;
 
 import java.math.BigInteger;
-import java.util.Random;
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.util.Arrays;
 
 import util.Util;
-import circuit.config.Config;
 import circuit.eval.CircuitEvaluator;
+import circuit.structure.ConstantWire;
+import circuit.structure.BitWire;
 import circuit.structure.CircuitGenerator;
 import circuit.structure.Wire;
 import circuit.structure.WireArray;
-import examples.gadgets.hash.MerkleTreePathGadget;
+import circuit.config.Config;
+import examples.gadgets.diffieHellmanKeyExchange.ECGroupGeneratorGadget;
 import examples.gadgets.hash.SubsetSumHashGadget;
+import examples.gadgets.hash.MerkleTreePathGadget;
+import examples.gadgets.diffieHellmanKeyExchange.ECGroupOperationGadget;
 
 
 public class Vote extends CircuitGenerator {
-
-	public static int mode = 0;
 	/********************* INPUT ***************************/
-	private Wire[][] pp;
+	private Wire G;
+	private Wire U;
 	private Wire[] E_id;
 	/********************* OUTPUT ***************************/
 	private Wire[] root;
@@ -40,7 +32,11 @@ public class Vote extends CircuitGenerator {
 	/********************* Witness ***************************/
 	private Wire[] SK_id;
 	private Wire[][] EK_id;
+
 	private Wire candidate;
+	private Wire Rho; 
+	
+
 
 	/********************* Vote Msg and random ***************************/
 	private Wire randomizedEnc;
@@ -56,106 +52,112 @@ public class Vote extends CircuitGenerator {
 	private int treeHeight;
 	
 	private int hashDigestDimension = SubsetSumHashGadget.DIMENSION;
-	private SubsetSumHashGadget subsetSumHashGadget;
-	private MerkleTreePathGadget merkleTreeGadget;
+	
+
+	public static final int EXPONENT_BITWIDTH = 253; // in bits
 
 	public Vote(String circuitName, int treeHeight) {
 		super(circuitName);
 		this.treeHeight = treeHeight;
-		this.num_of_elector = 64;
-		this.leafNumOfWords = 8;
-		this.leafWordBitWidth = 32;
-
 	}
 
-	public Wire power(Wire input, Wire exp) {
-		Wire zeroWire = createConstantWire(new BigInteger("0"));
-		Wire oneWire = createConstantWire(new BigInteger("1"));
-		Wire res = createConstantWire(new BigInteger("1"));
-		int index = 0;
-
-		Wire[] getBitExp = exp.getBitWires(256).asArray();
-		for (int i = 0; i < 256; i++) {
-			Wire tmp = input.sub(1);
-			tmp = tmp.mul(getBitExp[i]);
-			tmp = tmp.add(1);
-
-			res = res.mul(tmp);
-
-			exp = exp.shiftRight(1, 256);
-			input = input.mul(input);
+	public Wire[] expwire(Wire input){
+		Wire zerobitWire = createConstantWire(new BigInteger("0")).getBitWires(1).get(0);
+		Wire onebitWire = oneWire.getBitWires(1).get(0);
+		Wire[] temp = input.getBitWires(EXPONENT_BITWIDTH-3).asArray();
+		Wire[] output = new Wire[EXPONENT_BITWIDTH];
+		output[0] = zeroWire;
+		output[1] = zeroWire;
+		output[2] = zeroWire;
+		for(int i = 3 ; i < EXPONENT_BITWIDTH  ; i++)
+			output[i] = temp[i-3];
+		output[EXPONENT_BITWIDTH - 1] = oneWire;
+		for(int i = 0 ; i < output.length ; i++){
+			addBinaryAssertion(output[i], Integer.toString(i));
 		}
-		return res;
+		return output;
 	}
 
 	@Override
-	protected void buildCircuit() {      
-		/* output */
-		// root = new Wire[2][];
-		// sn = new Wire[3][]; 
-		VCT = new Wire[2][8];
-		pp = new Wire[2][];
+	protected void buildCircuit() {	
+		
 		EK_id = new Wire[2][];
 
-		/* Witness */
-		E_id = createInputWireArray(leafNumOfWords, "e_id"); // 투표 번호
-		for(int i = 0 ; i < 2 ; i++)
-		{
-			pp[i] = createInputWireArray(leafNumOfWords, "pp" + Integer.toString(i));
-			EK_id[i] = createProverWitnessWireArray(leafNumOfWords, "ek_id" + Integer.toString(i)); // S = G ^ s , T = (S ^ rho) * (G ^ real)		
+		E_id = createInputWireArray(leafNumOfWords, "E_id");
+		for(int i = 0 ; i < 2 ; i++){
+			EK_id[i] = createProverWitnessWireArray(leafNumOfWords, "ek_id" + Integer.toString(i));
 		}
+		// The secret exponent is a private input by the prover
+		// Rho = createProverWitnessWire("Rho");
+		// Wire g = createConstantWire(new BigInteger("16377448892084713529161739182205318095580119111576802375181616547062197291263"));;
+		// Wire h = createConstantWire(new BigInteger("8252578783913909531884765397785803733246236629821369091076513527284845891757"));
+		G = createInputWire("G");
+		U = createInputWire("U");
+		
 		directionSelector = createProverWitnessWire("Direction selector");
 		candidate = createProverWitnessWire("candidate"); // 후보자
 		SK_id = createProverWitnessWireArray(leafNumOfWords, "sk_id"); // voter private key
 		randomizedEnc = createProverWitnessWire("r");
+
+		// Wire[] rhobit = expwire(Rho);
+		Wire[] rbit = expwire(randomizedEnc);
+		Wire[] msgbit = expwire(candidate);
+		
+		// ECGroupGeneratorGadget exchange = new ECGroupGeneratorGadget(G, rhobit);
+		// U = exchange.getOutputPublicValue();
 		
 		Wire[] skBits = new WireArray(SK_id).getBits(leafWordBitWidth).asArray();
-		// System.out.println("ww : " + skBits.length);
-        subsetSumHashGadget = new SubsetSumHashGadget(skBits, false);
-		Wire[] PK_id = subsetSumHashGadget.getOutputWires();
-		// makeOutputArray(PK_id, "PK_id");
+		// System.out.println("skbits : " + skBits.length);
 
+		//2   *    32 * 8  => 256하나로
+
+		SubsetSumHashGadget hash = new SubsetSumHashGadget(skBits, false);
+		Wire[] PK_id = hash.getOutputWires();
+	
+		//비트수 맞출것
 		Wire[] sn_input = Util.concat(Util.concat(Util.concat(E_id, SK_id), EK_id[0]),EK_id[1]); //sn = H(E_ID||SK_ID||EK_ID)
 		//32 * 8 + 32 * 8 + 32 * 8 + 32 * 8
 
 		Wire[] snBits = new WireArray(sn_input).getBits(leafWordBitWidth).asArray();
 		// System.out.println("WW:"+snBits.length);
-		subsetSumHashGadget = new SubsetSumHashGadget(snBits, false);
-		sn = subsetSumHashGadget.getOutputWires();
+		hash = new SubsetSumHashGadget(snBits, false);
+		sn = hash.getOutputWires();
 		makeOutputArray(sn, "sn");
-
+		
 		intermediateHasheWires = createProverWitnessWireArray(hashDigestDimension * treeHeight, "Intermediate Hashes");
-
+		
 		Wire[] ek = Util.concat(EK_id[0],EK_id[1]);
 		// 32 * 8 + 32 * 8 +  32 * 3
 		Wire[] ekbits = new WireArray(ek).getBits(leafWordBitWidth).asArray();
-		subsetSumHashGadget = new SubsetSumHashGadget(ekbits, false);
-		Wire[] hashek = subsetSumHashGadget.getOutputWires();
+		hash = new SubsetSumHashGadget(ekbits, false);
+		Wire[] hashek = hash.getOutputWires();
 		Wire[] ekpk = Util.concat(hashek, PK_id);
-		// System.out.println("WW:"+ekpkBits.length);
-		merkleTreeGadget = new MerkleTreePathGadget(directionSelector, ekpk, intermediateHasheWires, 254, treeHeight);
+		// System.out.println("WW:"+ekpk.length);
+		MerkleTreePathGadget merkleTreeGadget = new MerkleTreePathGadget(directionSelector, ekpk, intermediateHasheWires, 254, treeHeight);
 		root = merkleTreeGadget.getOutputWires();
 		makeOutputArray(root, "Root");
-
-		// merkleTreeGadget = new MerkleTreePathGadget(directionSelector, PK_id, intermediateHasheWires, leafWordBitWidth, treeHeight);
-		// root[1] = merkleTreeGadget.getOutputWires();
-		// makeOutputArray(root[1], "Root[1]");
-
-		for(int i = 0 ; i < leafNumOfWords ; i++){
-			VCT[0][i] = (power(pp[0][i], randomizedEnc)).mul(power(EK_id[0][i], candidate));
-			VCT[1][i] = (power(EK_id[1][i], candidate)).mul(power(pp[1][i], randomizedEnc));
-		}
-
-		makeOutputArray(VCT[0], "vct[0]");
-		makeOutputArray(VCT[1], "vct[1]");
+		//  2^8
+		Wire S = new WireArray(EK_id[0]).getBits(32).packAsBits(256, "S");
+		Wire T = new WireArray(EK_id[1]).getBits(32).packAsBits(256, "T");
+		
+		S = createConstantWire(new BigInteger("8252578783913909531884765397785803733246236629821369091076513527284845891757"), "S");
+		T = createConstantWire(new BigInteger("8252578783913909531884765397785803733246236629821369091076513527284845891757"), "T");
+		ECGroupOperationGadget enc = new ECGroupOperationGadget(G, rbit, S, msgbit);
+		Wire V = enc.getOutputPublicValue();
+		enc = new ECGroupOperationGadget(U, rbit, T, msgbit);
+		Wire W = enc.getOutputPublicValue();
+		makeOutput(V, "V");
+		makeOutput(W, "W");
 
 	}
 
 	@Override
 	public void generateSampleInput(CircuitEvaluator circuitEvaluator) {
+		circuitEvaluator.setWireValue(G, new BigInteger("16377448892084713529161739182205318095580119111576802375181616547062197291263"));
+		circuitEvaluator.setWireValue(U, new BigInteger("4810973129926219629753993601758293538333634865847711720450943669829716657070"));
+		// circuitEvaluator.setWireValue(Rho, Util.nextRandomBigInteger(250));
 		for(int i = 0 ; i < 2 ; i++){
 			for(int j = 0 ; j < leafNumOfWords ; j++){
-				circuitEvaluator.setWireValue(pp[i][j], Integer.MAX_VALUE);
 				circuitEvaluator.setWireValue(EK_id[i][j], Integer.MAX_VALUE);
 			}
 		}
@@ -163,29 +165,30 @@ public class Vote extends CircuitGenerator {
 			circuitEvaluator.setWireValue(E_id[i], Integer.MAX_VALUE);
 			circuitEvaluator.setWireValue(SK_id[i], Integer.MAX_VALUE);
 		}
+
 		circuitEvaluator.setWireValue(directionSelector, Util.nextRandomBigInteger(treeHeight));
 		circuitEvaluator.setWireValue(candidate, Integer.MAX_VALUE);
 		circuitEvaluator.setWireValue(randomizedEnc, 1);
 		for (int i = 0; i < hashDigestDimension * treeHeight; i++) { 
 			circuitEvaluator.setWireValue(intermediateHasheWires[i], Integer.MAX_VALUE);
 		}
+		
 	}
 
-	public void setup()
-    {
-        this.generateCircuit();
-        this.evalCircuit();
-        this.prepFiles();
-        this.runLibsnark();
-    }
+	public BigInteger Generator() {
+		BigInteger g = Util.nextRandomBigInteger(254);
 
+		while (g.gcd(Config.FIELD_PRIME).compareTo(BigInteger.ONE) == 1)
+			g = Util.nextRandomBigInteger(254);
+		return g;
+	}
 	public static void main(String[] args) throws Exception {
-		Vote generator = new Vote("vote", 16);
+
+		Vote generator = new Vote("Vote", 16);
 		generator.generateCircuit();
 		generator.evalCircuit();
 		generator.prepFiles();
 		generator.runLibsnark();
-
 	}
 
 }
